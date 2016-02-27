bde2b9a479ea0141987c093d74b1adc454dfc253
Markus Gerstel <Anthchirp@users.noreply.github.com>
2016-02-27 16:21:43 +0000

    package definitions

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="postfix-quota-policyd",
+  author="M Gerstel",
+  version="0.1",
+  packages=find_packages(),
+  install_requires=[],
+)
