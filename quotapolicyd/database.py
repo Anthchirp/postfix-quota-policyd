0c234e8aca89d70d37120c9a474b44ba202895fe
Markus Gerstel <Anthchirp@users.noreply.github.com>
2016-02-27 22:24:19 +0000

    prototypes of the 5 required sql functions

--- quotapolicyd/database.py
+++ quotapolicyd/database.py
@@ -26,7 +26,38 @@ class db_link():
   def is_connected(self):
     return self._connected
 
+
+  def read_user_information(self, username):
+    '''SELECT username,
+       smtplogin.username IS NULL AS unseen,
+       smtplogin.locked,
+       smtplogin.password != auth.password AS reset,
+       smtplogin.authcount,
+       smtplogin.limit,
+       smtplogin.dynlimit,
+       smtplogin.lastseen
+  FROM auth LEFT OUTER JOIN smtplogin USING (username, source) WHERE username='$username'");'''
+    pass
+
+  def create_user(self, username):
+    '''INSERT INTO smtplogin (username, source, password, authcount, lastseen) SELECT username, source, password, 1, NOW() FROM auth WHERE username = '$username' '''
+    pass
+
+  def unlock_user(self, username):
+    '''UPDATE smtplogin SET locked = 'N', dynlimit = $newlimit, password = (SELECT password FROM auth WHERE username='$username') WHERE username='$username' '''
+    pass
+
+  def increment_user_counter(self, username):
+    '''UPDATE smtplogin SET authcount = authcount + 1, lastseen = NOW() WHERE username='$username' '''
+    pass
+
+  def lock_user(self, username):
+    '''UPDATE smtplogin SET authcount = authcount + 1, lastseen = NOW(), locked = 'Y' WHERE username='$username' '''
+    pass
+
+
   def _set_parameter(self, option, opt, value, parser):
+    '''callback function for optionparser'''
     if opt == '--db-host':
       self._db_host = value
     elif opt == '--db-port':
@@ -39,6 +70,7 @@ class db_link():
       self._db_db = value
 
   def add_command_line_options(self, optparser):
+    '''function to inject command line parameters'''
     optparser.add_option('--db-host', metavar='HOST',
       default=self._db_host,
       help='MySQL host address, default %default',
